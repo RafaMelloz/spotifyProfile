759805f7ce904f776bcbcbcfc79f69263ca564a7
RafaMelloz <rafaelmeloalvessouza@gmail.com>
2024-10-19 23:48:53 -0300

    icone na tela principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { signIn, useSession, signOut } from "next-auth/react";
+import { FaSpotify } from "react-icons/fa";
 
 export default function Home() {
   const { status } = useSession()
@@ -14,10 +15,10 @@ export default function Home() {
 
   return (
     <section className="flex justify-center flex-col gap-5 items-center h-full">
-      <h2 className="text-4xl font-semibold text-white">Spotify Perfil</h2>
+      <h2 className="text-4xl font-bold text-white">Spotify Perfil</h2>
 
-      <button onClick={handleLogin} className="text-black font-semibold bg-spotify px-4 py-2 rounded-full text-base hover:scale-105 transition-all duration-300">
-        Logar com Spotify
+      <button onClick={handleLogin} className="text-black font-semibold bg-spotify px-4 py-2 rounded-full text-base hover:scale-105 transition-all duration-300 flex gap-2 items-center">
+        <FaSpotify size={26}/> <span>Logar com Spotify</span>
       </button>
     </section>
   );
